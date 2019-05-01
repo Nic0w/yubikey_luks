64ae22588f09f508f039d602790248408c45b5a8
Nic0w <nic0w@pluto>
2019-05-01 10:45:51 +0200

    Added a function to output text either with echo or Plymouth.

--- verysecureboot_unlockscript.sh
+++ verysecureboot_unlockscript.sh
@@ -4,11 +4,22 @@ set -e
 export TPM2TOOLS_TCTI="device:/dev/tpm0"
 export OPENSSL_CONF="/conf/openssl_engine.conf"
 
+message()
+{
+	if [ -x /bin/plymouth ] && plymouth --ping; then
+		plymouth message --text="$*"
+	else
+	        echo "$@" >&2
+	fi
+	return 0
+}
+
+
 ENCRYPTED_SECRET="/conf/secret.encrypted"
 
-pcscd --foreground > /dev/null 2>&1 &
+pcscd --foreground > /dev/null 2>&1
 
-echo "Touch the Yubikey to boot." >&2
+message "Touch the Yubikey to boot."
 
 SECRET_DECRYPTED=`mktemp`
 openssl rsautl -engine pkcs11 -keyform engine -decrypt -inkey 3 -in $ENCRYPTED_SECRET -out $SECRET_DECRYPTED 1>&2
@@ -19,10 +30,10 @@ if [ $RESULT -eq 0 ];
 then
   cat $SECRET_DECRYPTED
   rm $SECRET_DECRYPTED
-  echo "Success!" >&2
+  message "Success!"
   exit
 fi
 
-echo "Failed to decrypt! Going into recovery mode." >&2
+message "Failed to decrypt! Going into recovery mode." >&2
 
 /lib/cryptsetup/askpass "Unlocking the disk fallback $CRYPTTAB_SOURCE ($CRYPTTAB_NAME)\nEnter passphrase: "
